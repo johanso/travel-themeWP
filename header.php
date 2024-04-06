f5e0b192a3daa90ed238bb5d14fd92389c8c8877
Johanso <johanricardo4@gmail.com>
2024-04-05 19:38:59 -0500

    add favicon

--- header.php
+++ header.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/images/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/images/site.webmanifest">
+  <meta name="msapplication-TileColor" content="#002f70">
+  <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
