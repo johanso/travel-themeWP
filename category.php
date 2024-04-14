4be292b22598cf34934f32fedc5cd742886e442e
Johanso <johanricardo4@gmail.com>
2024-04-13 19:15:25 -0500

    add category breadcrumb

--- category.php
+++ category.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
   <?php $category = get_queried_object(); ?>
   <div class="container">
+    <div class="post_header-breadcrumb">
+      <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
+    </div>
     <h1 class="title-page">Más Sobre: <span><?php echo $category->name; ?></span></h1>
     <p><?php echo $category->description; ?></p>
     <div class="blog-container">
